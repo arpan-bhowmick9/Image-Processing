368ae0bd45f9f1c329e6bc4ac789b91489b38828
Arpan Bhowmik <arpan.bhowmick9@gmail.com>
2018-05-24 23:49:08 +0530

    Changed The Colour Of The Rectangle In FaceDetection To Yellow

--- Face Detection/FaceDetection.py
+++ Face Detection/FaceDetection.py
@@ -10,7 +10,7 @@ while True:
 	gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 	faces=face_cascade.detectMultiScale(gray, 1.3, 5)
 	for(x,y,w,h) in faces:
-		cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+		cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,255), 2)
 	cv2.imshow('FaceDetection',img)
 	k=cv2.waitKey(30) & 0xff
 	if k==27:
